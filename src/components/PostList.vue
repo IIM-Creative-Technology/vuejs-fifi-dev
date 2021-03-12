a64e6c66fbb776bdcdfb95480005e8aba60eb99c
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-03-12 18:30:01 +0100

    list of posts  component

---
+++ src/components/PostList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="post-list">
+      <div class="card">
+      <div class="card-header">
+        <!-- Image of the Post -->
+        <img src="@/assets/img/sample.jpg" alt="ordinateur ">
+      </div>
+      <div class="card-body">
+        <!-- Title of the post -->
+        <h1>The title</h1>
+        <!-- content of the post -->
+        <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio hic provident odio natus aperiam iste ipsa ad quae, aspernatur magni impedit voluptatibus omnis molestiae praesentium temporibus nulla corporis modi tenetur!</p>
+        <hr>
+        <!-- author details-->
+        <div class="author">
+          <!-- date -->
+          <p class="date">Posté le 9 mars 2021</p>
+          <!-- author id -->
+          <p class="creator">Par Author</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    //
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+.author{
+  display: flex;
+}
+
+.creator{
+  margin-left: 10px;
+}
+
+</style>
