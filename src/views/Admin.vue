2eb54760ff6aae8418958e35e25ee6531dc4b547
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-03-12 18:09:44 +0100

    Admin Page

---
+++ src/views/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      <edit-post-list></edit-post-list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EditPostList from "@/components/EditPostList.vue";
+
+export default {
+  name: "Home",
+  components: {
+    EditPostList
+  }
+};
+</script>
